4aa9a8e8705291f8e20edae3a7ba9ecfae8c35ea
George <george_roots@hotmail.com>
2022-01-16 22:19:48 -0300

    Fixed acess

--- src/main/java/com/serasa/api/security/BasicSecurityConfig.java
+++ src/main/java/com/serasa/api/security/BasicSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.serasa.api.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,6 +16,18 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder senhaEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/pessoa").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().cors()
+				.and().csrf().disable();
+
 	}
 
 	@Override
